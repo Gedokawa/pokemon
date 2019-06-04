bb7862914995c827480e863fad3ab3e79e00e9cc
Grzegorz Sumara <grzsum@wp.pl>
2019-06-04 14:13:30 +0200

    Modified evolves pokemon img and names

--- src/app/pokemon-catched/pokemon-catched.component.ts
+++ src/app/pokemon-catched/pokemon-catched.component.ts
@@ -14,34 +14,81 @@ export class PokemonCatchedComponent implements OnInit {
   name = null;
   evolve_to_second = null;
   evolve_to_third = null;
+  evoSprites = [];
+  evoNames = [];
+  evoses = [];
+  evolves = [];
 
-  evolvesPokemon(id){
-    this.evolve = null;
+  // evolvesPokemon(id){
+  //   this.evolve = null;
+  //   this.name = null;
+  //   this.evolve_to_third = null;
+  //   this.evolve_to_second = null;
+  //   if(id !== null && id !== ''){
+  //     fetch(`${this.base}evolution-chain/${id}`)
+  //     .then(response => response.json())
+  //     .then(responseJson => {
+  //     this.evolve = responseJson;
+  //     console.log(this.evolve);
+  //     if(this.evolve.chain.evolves_to.length  > 0 ){
+  //       this.evolve_to_second = this.evolve.chain.evolves_to[0];
+
+  //             // this.evolve_to_third = this.evolve_to_second.evolves_to[0];
+  //           if(this.evolve_to_second.evolves_to.length > 0){
+  //             this.evolve_to_third = this.evolve_to_second.evolves_to[0];
+  //           }
+
+  //     }
+  //     // console.log(this.evolve_to_third.evolves_to != []);
+  //     // console.log(this.evolve_to_third);
+
+  //   console.log(this.evolve.chain.species.url);
+  //   fetch(`${this.evolve.chain.species.url}`)
+  //   .then(response => response.json())
+  //   .then(evolve => console.log(evolve))
+  //   })
+  //   }
+  // }
+
+  changePokemon(id){
     this.name = null;
-    this.evolve_to_third = null;
-    this.evolve_to_second = null;
-    if(id !== null && id !== ''){
+    if(id == 'string') { this.name = null;}
+    if(id !== null && id !== '') {
       fetch(`${this.base}evolution-chain/${id}`)
       .then(response => response.json())
-      .then(responseJson => {
-      this.evolve = responseJson;
-      console.log(this.evolve);
-      if(this.evolve.chain.evolves_to.length  > 0 ){
-        this.evolve_to_second = this.evolve.chain.evolves_to[0];
-
-              // this.evolve_to_third = this.evolve_to_second.evolves_to[0];
-            if(this.evolve_to_second.evolves_to.length > 0){
-              this.evolve_to_third = this.evolve_to_second.evolves_to[0];
-            }
-
-      }
-     
-
-      // console.log(this.evolve_to_third.evolves_to != []);
-      // console.log(this.evolve_to_third);
-    })
+      .then(data => {
+        const api = "https://pokeapi.co/api/v2/pokemon/";
+        const first = data.chain;
+        let second;
+        let third;
+        let evos = [];
+        if(first) {
+          const e1 = fetch(`${api}${first.species.name}/`);
+          evos.push(e1);
+          second = first.evolves_to[0];
+        }
+        if(second){
+          const e2 = fetch(`${api}${second.species.name}/`);
+          evos.push(e2);
+          third = second.evolves_to[0];
+        }
+        if(third){
+          const e3 = fetch(`${api}${third.species.name}/`);
+          evos.push(e3);
+        }
+        Promise.all(evos)
+          .then(responses => Promise.all(responses.map(value => value.json())))
+          .then(dataList => {
+            const sprites = dataList.map(v => v.sprites.front_default);
+            const names = dataList.map(n => n.name);
+            this.evoSprites = sprites;
+            this.evoNames = names;
+            // console.log(this.evoSprites);
+            // console.log(this.evoNames);
+          })
+        // console.log(data);
+      })
     }
-    
   }
 
   constructor() {
